1263a1b83b626ff0df72984234efade021e01f25
duong1124 <dangduong1124@gmail.com>
2025-04-20 19:58:46 +0700

    [MODIFY] Delete Des Decrypt, rename variables and args of DES

--- src/algorithms/des.py
+++ src/algorithms/des.py
@@ -3,31 +3,33 @@ from utils.helpers import *
 from algorithms.base import CryptoAlgorithm
 
 class DES(CryptoAlgorithm):
-    def __init__(self, key_size: int = 32):
+    def __init__(self, key_size: int = 56):
         super().__init__()
         self.key_size = key_size
-        self.generate_key()
 
-    def encrypt(plaintext: str, key: str) -> str:
-        """Encrypt the plaintext using the DES algorithm."""
+    def encrypt(self, plaintext: str, key: str) -> str:
+        """Encrypt the plaintext using the DES algorithm.
+        Args:
+            plaintext (str): The plaintext to encrypt (64 bits)
+            key (str): The key to encrypt the plaintext with (56 bits)
+        Returns:
+            str: The encrypted ciphertext (64 bits)
+        """
         permuted_plaintext = des_permute(plaintext, des_initial_permutation, 64)
-        left, right = des_split(permuted_plaintext, 32)
+        left, right = split_half(permuted_plaintext)
+
+        # Generate the 16 keys for each round   
+        keys = des_key_generation(key)
+
         for round in range(16):
-            left, right = des_mixer(left, right, des_key_generation(key)[round])
+            left, right = des_mixer(left, right, keys[round])
             if round != 15:
-                left, right = des_swapper(left + right)
+                left, right = swap_half(left, right)
+
         combined = des_combine(left, right)
         permuted_ciphertext = des_permute(combined, des_final_permutation, 64)
         return permuted_ciphertext
     
-    def decrypt(ciphertext: str, key: str) -> str:
+    def decrypt(self, ciphertext: str, key: str) -> str:
         """Decrypt the ciphertext using the DES algorithm."""
-        permuted_ciphertext = des_permute(ciphertext, des_initial_permutation, 64)
-        left, right = des_split(permuted_ciphertext, 32)
-        for round in range(16):
-            left, right = des_mixer(left, right, des_key_generation(key)[round])
-            if round != 15:
-                left, right = des_swapper(left + right)
-        combined = des_combine(left, right)
-        permuted_plaintext = des_permute(combined, des_final_permutation, 64)
-        return permuted_plaintext
+        pass # TODO: Implement the decryption function
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,5 +2,5 @@
 Utility functions for the cryptography project.
 """ 
 
-from .helpers import hex2bin, bin2hex, bin2dec, dec2bin, hex2dec, dec2hex, generate_random_string, pad_data, unpad_data
+from .helpers import hex2bin, bin2hex, bin2dec, dec2bin, hex2dec, dec2hex, split_half, circular_shift_left, generate_random_string, pad_data, unpad_data
 from .des_helpers import *
