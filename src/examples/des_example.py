0f17cfd74f606d3a5f36f3296f97943cdb089299
duong1124 <dangduong1124@gmail.com>
2025-04-22 15:11:44 +0700

    [ADD] Examples folder for running algo, Update README

---
+++ src/examples/des_example.py
@@ -0,0 +1,14 @@
+from algorithms import DES
+
+from utils import hex2bin, bin2hex, bin2dec, dec2bin, hex2dec, dec2hex, split_half, circular_shift_left, generate_random_string, pad_data, unpad_data
+
+plaintext = "123456ABCD132536"
+plaintext = hex2bin(plaintext)
+
+key = "AABB09182736CCDD"
+keyb = bin2hex(key)
+
+des = DES()
+ciphertext = des.encrypt(plaintext, keyb)
+print(f"Ciphertext: {ciphertext}")
+decrypted_text = des.decrypt(ciphertext, keyb)
